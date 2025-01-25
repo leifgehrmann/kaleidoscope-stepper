7ee8789aab46ee133c59f30d04c5b0ef4e890aa3
Leif Gehrmann <leif.gehrmann@gmail.com>
2025-01-25 21:58:52 +0000

    Switched to using an SVG for showing the raytrace

--- src/components/RayTrace.vue
+++ src/components/RayTrace.vue
@@ -11,63 +11,82 @@ interface Point {
   y: number;
 }
 
-const canvasRef = useTemplateRef('canvas');
-const canvasSize =  Math.max(1024, window.innerWidth, window.innerHeight) * window.devicePixelRatio;
-const ctxRef = ref(null as null|CanvasRenderingContext2D);
+const viewbox = ref('0 0 100 100');
+const rayD = ref(null as string|null);
+const svgRef = useTemplateRef('svg');
 
 function render(rayPos: Point|null) {
-  const canvas = canvasRef.value as null | HTMLCanvasElement;
-  const ctx = ctxRef.value;
-  if (canvas === null || ctx === null) {
+  if (svgRef.value === null) {
     return;
   }
+  const svg = svgRef.value as SVGElement;
+  const svgRect = svg.getBoundingClientRect();
+  viewbox.value = `0 0 ${svgRect.width} ${svgRect.height}`;
 
   if (rayPos === null) {
-    ctx.clearRect(0,0, canvas.width, canvas.height)
+    rayD.value = null;
     return;
   }
 
-  ctx.beginPath();
-  ctx.fillStyle = 'blue';
-  ctx.moveTo(20, 20);
-  ctx.lineTo(180, 20);
-  ctx.lineTo(rayPos.x, rayPos.y);
-  ctx.closePath();
-  ctx.fill();
+  rayD.value = `M ${svgRect.width/2} ${svgRect.height/2} L ${rayPos.x} ${rayPos.y}`
+  // const canvas = canvasRef.value as null | HTMLCanvasElement;
+  // const ctx = ctxRef.value;
+  // if (canvas === null || ctx === null) {
+  //   return;
+  // }
+  //
+  // if (rayPos === null) {
+  //   ctx.clearRect(0,0, canvas.width, canvas.height);
+  //   return;
+  // }
+  //
+  // ctx.beginPath();
+  // ctx.fillStyle = 'blue';
+  // ctx.moveTo(20, 20);
+  // ctx.lineTo(180, 20);
+  // ctx.lineTo(rayPos.x, rayPos.y);
+  // ctx.closePath();
+  // ctx.fill();
 
   // ctx.clearRect(0,0, canvas.width, canvas.height)
 }
 
 watch(() => (props.maxReflections), () => {
-  render(null);
+  // render(null);
 });
 
 onMounted(() => {
-  const canvas = canvasRef.value as HTMLCanvasElement;
-  ctxRef.value = canvas.getContext('2d')!;
-  canvas.width = canvas.height = canvasSize;
+  const svg = svgRef.value as HTMLCanvasElement;
 
   render(null);
 
-  canvas.addEventListener('mousemove', (e) => {
+  window.addEventListener('mousemove', (e) => {
     render({x: e.clientX, y: e.clientY});
   });
 });
 
 window.addEventListener('resize', () => {
-  requestAnimationFrame(() => {
-    render(null);
-  });
+  render(null);
 });
 
 </script>
 
 <template>
-  <canvas
-    ref="canvas"
+  <svg
+    ref="svg"
     class="absolute left-0 top-0"
     style="width:100dvw;height:100dvh;"
-  />
+    :viewBox="viewbox"
+  >
+    <path
+      v-if="rayD !== null"
+      :d="rayD"
+      stroke="#FFF"
+      stroke-width="3"
+      stroke-opacity="1"
+      stroke-linecap="round"
+    />
+  </svg>
 </template>
 
 <style scoped>
